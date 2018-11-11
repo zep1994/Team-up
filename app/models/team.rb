cbed0c8abf3d53e5f2adc452a2f257b070cdf2b1
zep1994 <snaxbags@protonmail.com>
2018-11-11 16:17:30 -0600

    Add assignment attributes to team model to get team new to work with respect to assignments

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,5 +3,17 @@ class Team < ApplicationRecord
   has_many :players, through: :assignments
 
   #validations
-  validates_presence_of :name 
+  validates_presence_of :name
+
+
+  def assignments_attributes=(assignments_attributes)
+      assignments_attributes.values.each do |assignment_attributes|
+          if assignment_attributes[:team_id] != ""
+              assignment = Assignment.find_or_create_by(assignment_attributes)
+              if !self.assignments.include?(assignment)
+                  self.assignments.build(assignment_attributes)
+              end
+          end
+      end
+  end
 end
