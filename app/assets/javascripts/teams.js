bebebcf7d81fd64a5a2dec9ae9efb310052c5dcf
zep1994 <snaxbags@protonmail.com>
2018-11-26 23:00:17 -0600

    Add get teams method in javascripts/teams.js

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -6,8 +6,32 @@ class Team {
     this.role = attr.role
     this.quote = attr.quote
   }
+  renderTeam() {
+    return 'Name: <a href="#" onclick="teamData(' + this.id + ')">' + this.name + '</a>'
+  }
 }
 
 let allTeams = []
 
-function 
+function getTeams() {
+  const teamsList = document.querySelector('#listArea')
+  const teamsUL = document.createElement('ul')
+  teamsUL.setAttribute('id', 'teamsList')
+  teamsList.innerHTML = "<h3>Teams</h3>"
+  teamsList.appendChild(teamsUL)
+
+  const req = new XMLHttpRequest()
+  allTeams = []
+  req.open('GET', '/teams')
+  req.responseType = 'json'
+  req.send()
+  req.onload = function() {
+    req.response.forEach(function(i){
+      let team = new Team(i)
+      allTeams.push(team)
+      let teamLi = document.createElement('li')
+      teamLi.innerHTML = team.renderTeam()
+      teamsUL.appendChild(teamLi)
+    })
+  }
+}
