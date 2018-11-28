06df0c9a97c9729d26f7d943db6425b4cc547a6a
zep1994 <snaxbags@protonmail.com>
2018-11-28 09:09:11 -0600

    Fix moreData.innerHTML line

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -45,6 +45,6 @@ function teamData(team_id) {
   req.send()
   req.onload = function() {
     let team = new Team(req.response)
-    moreData.innerHTML = '<br><h3>Additional Information for '+team.name+'</h3><p><strong>Type of Player: </strong>'+team.type_player+', <strong>Note: </strong>'+team.quote+'</p><a href="/teams/'+team.id+'">Manage Team</a>';
+    moreData.innerHTML = '<br><h3>Additional Information for '+team.name+'</h3><p><strong>Role: </strong>'+team.role+', <strong>Note: </strong>'+team.quote+'</p><a href="/teams/'+team.id+'">Manage Team</a>';
   }
 }
